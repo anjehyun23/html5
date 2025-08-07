1e5c15dd1e87820e5338d3c2827ce5be54632acd
anjehyun23 <anjehyun23@lx.or.kr>
2025-08-07 16:38:54 +0900

    humortube 완성

---
+++ humortube/src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  
+ return (
+<>
+  <div className="center_div">
+    
+    <h2>인천-오사카</h2>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/5wm7Ts_JJco?si=7bNJQZXZz50Mgykg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+   <h2>즐거운추석</h2>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/pmubngOzFLc?si=W0grJLkSSpEaYJjx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+   <h2>전참시</h2>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/jyxNeareLFw?si=7ErALz_U9sj09ZpT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+  </div>
+ </>
+  )
+}
+
+export default App
